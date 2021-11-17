c28de9af88548fbbc1652313992afce7714a1abf
rohanailoni <rohanailoni@gmail.com>
2021-11-17 12:06:45 +0400

    updated Gitigonere and added new canny model to the file

--- steganography.py
+++ steganography.py
@@ -38,11 +38,9 @@ class Steganography:
         if img2.size[0] > img1.size[0] or img2.size[1] > img1.size[1]:
             raise ValueError('Image 2 should not be larger than Image 1!')
 
-        # Get the pixel map of the two images
         pixel_map1 = img1.load()
         pixel_map2 = img2.load()
 
-        # Create a new image that will be outputted
         new_image = Image.new(img1.mode, img1.size)
         pixels_new = new_image.load()
 
@@ -50,14 +48,11 @@ class Steganography:
             for j in range(img1.size[1]):
                 rgb1 = Steganography.__int_to_bin(pixel_map1[i, j])
 
-                # Use a black pixel as default
                 rgb2 = Steganography.__int_to_bin((0, 0, 0))
 
-                # Check if the pixel map position is valid for the second image
                 if i < img2.size[0] and j < img2.size[1]:
                     rgb2 = Steganography.__int_to_bin(pixel_map2[i, j])
 
-                # Merge the two pixels and convert it to a integer tuple
                 rgb = Steganography.__merge_rgb(rgb1, rgb2)
 
                 pixels_new[i, j] = Steganography.__bin_to_int(rgb)
@@ -80,24 +75,18 @@ class Steganography:
 
         for i in range(img.size[0]):
             for j in range(img.size[1]):
-                # Get the RGB (as a string tuple) from the current pixel
                 r, g, b = Steganography.__int_to_bin(pixel_map[i, j])
 
-                # Extract the last 4 bits (corresponding to the hidden image)
-                # Concatenate 4 zero bits because we are working with 8 bit
+
                 rgb = (r[4:] + '0000',
                        g[4:] + '0000',
                        b[4:] + '0000')
 
-                # Convert it to an integer tuple
                 pixels_new[i, j] = Steganography.__bin_to_int(rgb)
 
-                # If this is a 'valid' position, store it
-                # as the last valid position
                 if pixels_new[i, j] != (0, 0, 0):
                     original_size = (i + 1, j + 1)
 
-        # Crop the image based on the 'valid' pixels
         new_image = new_image.crop((0, 0, original_size[0], original_size[1]))
 
         return new_image
